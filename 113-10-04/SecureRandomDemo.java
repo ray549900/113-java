d850a3d7e93535546e6359964c7f6aa519288e10
A112223015 <b212190218@gmail.com>
2024-10-04 11:59:07 +0800

    Add files via upload

---
+++ 113-10-04/SecureRandomDemo.java
@@ -0,0 +1,19 @@
+
+import java.security.SecureRandom;
+
+public class SecureRandomDemo {
+    public static void main(String[] args) {
+        SecureRandom randomNumber = new SecureRandom();
+        while(true) {
+            var number = randomNumber.nextInt(bound:10);
+            System.out.println(number);
+            
+            if(number == 5) {
+                System.out.println("I hit 5....Orz");
+                break;
+            }
+        }
+    }
+}
+
+
