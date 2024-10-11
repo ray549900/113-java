24134afac5079bf81f50632d9b73d6c97d27d15d
A112223015 <b212190218@gmail.com>
2024-10-11 11:58:06 +0800

    Add files via upload

---
+++ 113-10-11/Sum.java
@@ -0,0 +1,20 @@
+import java.awt.im.InputContext;
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        var console = new Scanner(System.in);
+        
+        var sum = 0;
+        var number = 0;
+        do { 
+            System.out.print(s:"輸入數字");
+            number = Integer.parseInt(input.nextLine());
+            sum += number;
+            
+        } while (number !=0);
+
+             
+        System.out.println("總合：" + sum);
+    }
+}
