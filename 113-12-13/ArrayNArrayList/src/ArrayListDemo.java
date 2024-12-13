3632b94783e212315e043fee6531c174be32770b
A112223015 <b212190218@gmail.com>
2024-12-13 12:01:12 +0800

    Add files via upload

---
+++ 113-12-13/ArrayNArrayList/src/ArrayListDemo.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    ArrayList<String>list=new ArrayList<>();
+
+    list.add("apple");
+    list.add("Banana");
+    list.add("Cherry");
+
+    System.out.println("ArryList內容"+ list);
+
+    String fruit = list.get;
+}
